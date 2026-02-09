4d6e83810d615e3f853dce0dbee814c51c53c7fd
subugee <sasubramanian_md@hotmail.com>
2026-02-09 16:03:11 +0530

    camel direct:start and logger

--- camelplainjavaapp/camelJavaApp/src/main/java/com/techm/camel/LoggingComponent.java
+++ camelplainjavaapp/camelJavaApp/src/main/java/com/techm/camel/LoggingComponent.java
@@ -5,27 +5,27 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class LoggingDSL {
+public class LoggingComponent {
     public static void main(String[] args) throws Exception {
         CamelContext context = new DefaultCamelContext();
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() {
                 from("direct:start")
-                        .log("Incoming message: ${body}")
-                        .log("Headers: ${headers}")
-                        .log("Exchanged Id ${exchangeId}")
+                        //log is component; incoming; end point;?variableName=value is query string
+                        .to("log:incoming?level=INFO&showBody=true")
+                        .to("log:headers?level=INFO&showHeaders=true")
+                        .to("log:exchange?level=INFO&showExchangeId=true")
                         .to("mock:end");
-
             }
         });
 
         context.start();
         //ProducerTemplate is object used to send message to direct:start
         //sending message from outside of camel datasources
-        ProducerTemplate producerTemplate =context.createProducerTemplate();
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
 //        producerTemplate.sendBody("direct:start","Hello Camel");
-        producerTemplate.sendBodyAndHeader("direct:start","This is body","myheader","HelloCamel");
+        producerTemplate.sendBodyAndHeader("direct:start", "This is body", "myheader", "HelloCamel");
 
 
         // Keep app alive
